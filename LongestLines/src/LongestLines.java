1472dd2965867e909bcc302a0a915074b45766db
viralsavani <viralsavani@gmail.com>
2014-12-27 19:05:17 -0800

    Using Comparator

--- LongestLines/src/LongestLines.java
+++ LongestLines/src/LongestLines.java
@@ -1,20 +1,25 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.Comparator;
 
 
-public class LongestLines {
+public class LongestLines implements Comparator<String>{
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 
-		String[] input = {"2","Hello World","CodeEval", "Quick Fox", "A", "San Francisco"};
-		int linesToPrint = Integer.parseInt(input[0]);
-		ArrayList<String> stringList = new ArrayList<>();
-		
-		for (int i = 1; i < input.length; i++) {
-			stringList.add(input[i]);			
-		}
-		Collections.sort(stringList,Collections.reverseOrder());
+		String[] input = {"2","Hello World","CodeEval","Quick Fox","A","San Francisco"};
+		System.out.println("Input: " + Arrays.toString(input));//before sorting by length
+		Arrays.sort(input, new LongestLines());
+		System.out.println("Array Sorted:" + Arrays.toString(input));
+	}
+
+	@Override
+	public int compare(String o1, String o2) {
+		if (o1.length() > o2.length())
+			return -1;
+		else if (o1.length() < o2.length())
+			return 1;
+		else return 0;
 	}
 }
+
+
