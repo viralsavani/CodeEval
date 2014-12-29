2ae915cf778ebdabdba6a2c960b145688be2b558
viralsavani <viralsavani@gmail.com>
2014-12-29 15:17:19 -0800

    Sum of Digits. Simple

---
+++ SumOfInteger/src/SumOfInteger.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class SumOfInteger {
+
+	public static void main(String[] args) throws IOException {
+		File file = new File(args[0]);
+		BufferedReader buffer = new BufferedReader(new FileReader(file));
+		String line;
+		int sum = 0;
+		while ((line = buffer.readLine()) != null) {
+			line = line.trim();
+			sum = sum + Integer.parseInt(line);
+		}
+		System.out.println(sum);
+	}
+
+}
