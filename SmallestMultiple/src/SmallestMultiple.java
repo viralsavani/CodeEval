596f1830c485bfcd5f217468632789bee0fbb009
viralsavani <viralsavani@gmail.com>
2014-12-30 15:29:11 -0800

    Improved One. Case 0

--- SmallestMultiple/src/SmallestMultiple.java
+++ SmallestMultiple/src/SmallestMultiple.java
@@ -9,7 +9,7 @@ public class SmallestMultiple {
 		File file = new File(args[0]);
 		BufferedReader buffer = new BufferedReader(new FileReader(file));
 		String line;
-		int x, n;
+		int x, n, i=0;
 		int maxNumber = 0;
 		while ((line = buffer.readLine()) != null) {
 			line = line.trim();
@@ -17,10 +17,14 @@ public class SmallestMultiple {
 			x = Integer.parseInt(numberString[0]);
 			n = Integer.parseInt(numberString[1]);
 			
-			while(maxNumber <= x){
+			if(x == 0){
+				System.out.println(n);
+			}
+			while(maxNumber < x){
 				maxNumber = maxNumber + n;
 			}
 			System.out.println(maxNumber);
+			maxNumber = 0;
 		}
 		buffer.close();
 	}
