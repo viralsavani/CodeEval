ec1e536d4216da6a1a16a7eb597afafc5ea20186
Viral Savani <viralsavani@gmail.com>
2015-01-03 11:26:13 -0800

    Self Describing Number using only char and strings

--- SelfDescribingNumber/src/SelfDescribingNumber.java
+++ SelfDescribingNumber/src/SelfDescribingNumber.java
@@ -1,15 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class SelfDescribingNumber {
 
-	public static void main(String[] args) {
-		
-		String line = "1210";
-		int digits = line.length()-1;
-		int number = Integer.parseInt(line);
-		for(int i = digits; i >= 0; i--){
-			int lastDigit = number % 10;
-			number = number / 10;
-			System.out.println(i+" Should Occur "+lastDigit);
-		}	
+	public static void main(String[] args) throws IOException {
+
+
+		File file = new File(args[0]);
+		BufferedReader buffer = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = buffer.readLine()) != null) {
+			line = line.trim();
+
+			boolean flag = false;
+			for(int i = 0; i < line.length(); i++){
+
+				char firstChar = line.charAt(i);
+				//System.out.println(i+" OCCURS "+firstChar+" TIMES");
+
+				int digitCounter = 0;
+				for(int j = 0; j < line.length(); j++){
+					char tempChar = line.charAt(j);
+					if(((i+"").toString()).charAt(0) == tempChar){
+						//System.out.println("III"+i);
+						digitCounter++;
+					}
+				}
+				if(((digitCounter+"").toString()).charAt(0) == firstChar){
+
+					flag = true;
+					//System.out.println("FLAG :: "+flag);
+				}else{
+					flag = false;
+					break;
+				}
+			}
+			if(flag){
+				System.out.println(1);
+			}else{
+				System.out.println(0);
+			}
+		}
 	}
 }
